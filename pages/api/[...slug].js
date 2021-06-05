87db574541c51740ab5da0b2f069b28e37fb1e16
张文翔 <771797746@qq.com>
2021-06-05 17:24:05 +0800

    basic

---
+++ pages/api/[...slug].js
@@ -0,0 +1,28 @@
+import { transformSync, buildSync } from "esbuild"
+import { execSync } from "child_process"
+import fs from "fs"
+import path from "path"
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true)
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Content-Type", "application/javascript; charset=utf-8")
+
+  const { slug } = req.query
+  // TODO: Organization, Version ...
+  const packageName = slug[0]
+
+  execSync(`pnpm install ${packageName} --dir ${__dirname}`)
+  fs.writeFileSync(
+    path.resolve(__dirname, "in.js"),
+    `export { default } from "lodash"`
+  )
+  buildSync({
+    entryPoints: [path.resolve(__dirname, "in.js")],
+    outfile: path.resolve(__dirname, "out.js"),
+    bundle: true,
+    format: "esm",
+    minify: true,
+  })
+  const code = fs.readFileSync(path.resolve(__dirname, "out.js"), "utf8")
+  res.end(code)
+}
